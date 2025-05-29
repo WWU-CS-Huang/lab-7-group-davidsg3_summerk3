23d2b7b87d93c233782a3ba676ce99dce0a6c178
ChocolateChipPancake1 <gmiyt2@gmail.com>
2025-05-29 14:21:41 -0700

    doing it fr style

--- app/src/main/java/lab7/CodingTree.java
+++ app/src/main/java/lab7/CodingTree.java
@@ -1,4 +1,18 @@
 package lab7;
 
+import java.util.BitSet;
+import heap.Heap;
+
 public class CodingTree {
+    Node head;
+
+    CodingTree(Heap<Node, Integer> frequencies) {
+        while (frequencies.size() < 1) {
+            Node newGuy = new Node(frequencies.poll(), frequencies.poll());
+            frequencies.add(newGuy, newGuy.frequency);
+        }
+        head = frequencies.poll();
+    }
+
+    
 }
--- app/src/main/java/lab7/Node.java
+++ app/src/main/java/lab7/Node.java
@@ -1,4 +1,21 @@
 package lab7;
 
 public class Node {
+    Node left;
+    Node right;
+    int frequency;
+    char letter;
+    boolean terminates;
+
+    Node(int frequency, char letter) {
+        this.frequency = frequency;
+        this.letter = letter;
+        terminates = true;
+    }
+
+    Node(Node left, Node right) {
+        this.frequency = left.frequency + right.frequency;
+        this.left = left;
+        this.right = right;
+    }
 }
